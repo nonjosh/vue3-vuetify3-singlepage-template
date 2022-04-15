bd12714ca504dfec90be423c31deaf8cfb74ed40
nonjosh <nonjosh@gmail.com>
2022-04-15 18:41:52 +0000

    update webpage title

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,15 @@ module.exports = defineConfig({
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
+  },
+
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      title: 'Vuetify3 Single Page Template',
+    },
   }
 })
