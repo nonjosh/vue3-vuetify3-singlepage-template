ddb4d5d273cdc69d5dca1130306435abfd6d942c
nonjosh <nonjosh@gmail.com>
2022-04-15 18:42:28 +0000

    add sections as components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <LaunchSection />
+      <TestSection1 />
+      <TestSection2 />      
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LaunchSection from "./components/LaunchSection.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import TestSection1 from "./components/TestSection1.vue";
+import TestSection2 from "./components/TestSection2.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
+    LaunchSection,
     HelloWorld,
+    TestSection1,
+    TestSection2
   },
 
   data: () => ({
     //
   }),
-}
+};
 </script>
---
+++ src/components/TestSection2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="parallax"></div>
+</template>
+
+<style>
+.parallax {
+  /* The image used */
+  background-image: url("https://images.unsplash.com/photo-1429979787503-f2d7d20550c8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=905cec6df3d8ee5b1655bc19b980bae9&auto=format&fit=crop&w=1462&q=80");
+
+  /* Set a specific height */
+  min-height: 500px;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
