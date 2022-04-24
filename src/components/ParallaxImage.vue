8664382bde4906e42063327f62f79b97fec9a51c
nonjosh <nonjosh@gmail.com>
2022-04-24 07:17:34 +0000

    create ParallaxImage component

---
+++ src/components/ParallaxImage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="parallax" :style="cssProps"></div>
+</template>
+
+<script>
+export default {
+  name: "ParallaxImage",
+  props: {
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    cssProps() {
+      return {
+        "--background-image": `url(${this.imageUrl})`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.parallax {
+  /* The image used */
+  background-image: var(--background-image);
+
+  /* Set a specific height */
+  /* min-height: 500px; */
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
--- src/components/TestSection2.vue
+++ src/components/TestSection2.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="parallax"></div>
+  <ParallaxImage :image-url="imageUrl" style="height: 100vh" />
 </template>
 
-<style>
-.parallax {
-  /* The image used */
-  background-image: url("https://images.unsplash.com/photo-1429979787503-f2d7d20550c8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=905cec6df3d8ee5b1655bc19b980bae9&auto=format&fit=crop&w=1462&q=80");
+<script>
+import ParallaxImage from "@/components/ParallaxImage.vue";
 
-  /* Set a specific height */
-  min-height: 500px;
-
-  /* Create the parallax scrolling effect */
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-</style>
+export default {
+  components: {
+    ParallaxImage,
+  },
+  data() {
+    return {
+      imageUrl: `https://images.unsplash.com/photo-1429979787503-f2d7d20550c8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=905cec6df3d8ee5b1655bc19b980bae9&auto=format&fit=crop&w=1462&q=80`,
+    };
+  },
+};
+</script>
